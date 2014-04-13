7a32c29b2f6091b3b4abd86e3465fa282cff20cc
utnso <utnso@utnso>
2014-04-13 02:35:55 -0300

    Actualizacion Programa

--- Programa/Debug/src/commons/subdir.mk
+++ Programa/Debug/src/commons/subdir.mk
@@ -9,8 +9,10 @@ C_SRCS += \
 /home/utnso/git/tp-2014-1c-escriba-su-nombre-aqui/commons/commons/error.c \
 /home/utnso/git/tp-2014-1c-escriba-su-nombre-aqui/commons/commons/log.c \
 /home/utnso/git/tp-2014-1c-escriba-su-nombre-aqui/commons/commons/process.c \
+/home/utnso/git/tp-2014-1c-escriba-su-nombre-aqui/commons/commons/sockets.c \
 /home/utnso/git/tp-2014-1c-escriba-su-nombre-aqui/commons/commons/string.c \
 /home/utnso/git/tp-2014-1c-escriba-su-nombre-aqui/commons/commons/temporal.c \
+/home/utnso/git/tp-2014-1c-escriba-su-nombre-aqui/commons/commons/tools.c \
 /home/utnso/git/tp-2014-1c-escriba-su-nombre-aqui/commons/commons/txt.c 
 
 OBJS += \
@@ -19,8 +21,10 @@ OBJS += \
 ./src/commons/error.o \
 ./src/commons/log.o \
 ./src/commons/process.o \
+./src/commons/sockets.o \
 ./src/commons/string.o \
 ./src/commons/temporal.o \
+./src/commons/tools.o \
 ./src/commons/txt.o 
 
 C_DEPS += \
@@ -29,8 +33,10 @@ C_DEPS += \
 ./src/commons/error.d \
 ./src/commons/log.d \
 ./src/commons/process.d \
+./src/commons/sockets.d \
 ./src/commons/string.d \
 ./src/commons/temporal.d \
+./src/commons/tools.d \
 ./src/commons/txt.d 
 
 
@@ -70,6 +76,13 @@ src/commons/process.o: /home/utnso/git/tp-2014-1c-escriba-su-nombre-aqui/commons
 	@echo 'Finished building: $<'
 	@echo ' '
 
+src/commons/sockets.o: /home/utnso/git/tp-2014-1c-escriba-su-nombre-aqui/commons/commons/sockets.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -I"/home/utnso/git/tp-2014-1c-escriba-su-nombre-aqui/commons" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
 src/commons/string.o: /home/utnso/git/tp-2014-1c-escriba-su-nombre-aqui/commons/commons/string.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
@@ -78,6 +91,13 @@ src/commons/string.o: /home/utnso/git/tp-2014-1c-escriba-su-nombre-aqui/commons/
 	@echo ' '
 
 src/commons/temporal.o: /home/utnso/git/tp-2014-1c-escriba-su-nombre-aqui/commons/commons/temporal.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -I"/home/utnso/git/tp-2014-1c-escriba-su-nombre-aqui/commons" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/commons/tools.o: /home/utnso/git/tp-2014-1c-escriba-su-nombre-aqui/commons/commons/tools.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
 	gcc -I"/home/utnso/git/tp-2014-1c-escriba-su-nombre-aqui/commons" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
--- Programa/src/Programa.c
+++ Programa/src/Programa.c
@@ -4,56 +4,10 @@
 
 
 #include <sys/socket.h>
-//#include <sys/sendfile.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#include <sys/sendfile.h>
 
 #include "commons/config.h"
-
-int conectar(char *ip, int port) {
-	//PF_INET: IPv4
-	//SOCK_STREAM: Los datos no se pierden ni se duplican
-	//IPPROTO_TCP: TCP
-	int sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-
-	if (sock < 0)
-		return -1; //No se pudo crear el descriptor a un archivo
-
-	struct sockaddr_in sockaddress;
-
-	memset(&sockaddress, 0, sizeof(struct sockaddr_in));
-	sockaddress.sin_family = AF_INET;
-	sockaddress.sin_addr.s_addr = inet_addr(ip);
-	sockaddress.sin_port = htons(port);
-
-	if( connect(sock, (struct sockaddr*)&sockaddress, sizeof(struct sockaddr)))
-		return -1;  //No se pudo conectar
-
-	return sock;
-}
-
-char* LeerArchivo(char *archivo, size_t *size) {
-	FILE *f = fopen(archivo, "r");
-
-	if (f == NULL )
-		return NULL ; //No pudo abrir el archivo
-
-	//Obteniendo tamaño del archivo
-	fseek(f, 0, SEEK_END);
-	*size = ftell(f);
-	fseek(f, 0, SEEK_SET);
-
-	char *contenido = malloc(*size * sizeof(char));
-	if (contenido == NULL )
-		return NULL ; //Error de memoria
-
-	if (fread(contenido, 1, *size, f) != *size)
-		return NULL ; //No pudo leer todo el archivo
-
-	fclose(f);
-
-	return contenido;
-}
+#include "commons/sockets.h"
 
 int main(int argc, char *argv[]) {
 	if (argc != 2) {
@@ -61,15 +15,20 @@ int main(int argc, char *argv[]) {
 		return EXIT_SUCCESS;
 	}
 
-	size_t scriptSize;
-	char *script = LeerArchivo(argv[1], &scriptSize); //argv[1] = nombre del script a correr
+	t_config *config = config_create("$ANSISOP_CONFIG");
 
-	if (script == NULL) {
+	FILE *script = fopen(argv[1], "r"); //argv[1] = nombre del script a correr
+	size_t scriptSize;
+
+	if (script == NULL ) {
 		printf("Error al leer el script: %s\n", argv[1]);
 		return EXIT_SUCCESS;
 	}
 
-	t_config *config = config_create("$ANSISOP_CONFIG");
+	//Obteniendo tamaño del archivo
+	fseek(script, 0, SEEK_END);
+	scriptSize = ftell(script);
+	fseek(script, 0, SEEK_SET);
 
 	int sock = conectar(config_get_string_value(config, "IP"), config_get_int_value(config, "Puerto"));
 
@@ -78,13 +37,18 @@ int main(int argc, char *argv[]) {
 		return EXIT_SUCCESS;
 	}
 
-	send(sock, &script, scriptSize, 0);
+	//send(sock, header, sizeof(header);
+
+	if( sendfile(sock, fileno(script), 0, scriptSize) < 0 ) {
+		printf("No se pudo enviar el archivo.\n");
+		return EXIT_SUCCESS;
+	}
 
 	//while( recv(sock, output) ) printf(“%s\n”, output);
 
 	close(sock);
+	fclose(script);
 	config_destroy(config);
-	free(script);
 
 	return EXIT_SUCCESS;
 }
