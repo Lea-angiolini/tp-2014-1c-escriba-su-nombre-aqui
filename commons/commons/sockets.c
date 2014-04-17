ef06625a169ccab0b0cf92a8bc165dbf7c37f627
FernandoVelcic <fernando_velcic_77@hotmail.com>
2014-04-17 13:20:11 -0300

    Nueva caracteristica CrearServidorNoBloqueante

--- commons/commons/sockets.c
+++ commons/commons/sockets.c
@@ -1,13 +1,15 @@
 #include "log.h"
 #include "sockets.h"
 
-
 #include <string.h>
+#include <stdlib.h>
+
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-
-
+#include <pthread.h>
+#include <sys/ioctl.h>
+#include <errno.h>
 
 /*
  * Funcion que crea un socket cliente para conectarlo a la ip y puerto pasados como parametros
@@ -21,7 +23,7 @@ int conectar(char *ip, int port, t_log * log) {
 	log_info(log, "Conectando");
 	int sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-	if (sock < 0){
+	if (sock < 0) {
 		log_error(log, "Error al crear el socket");
 		return -1; //No se pudo crear el descriptor a un archivo
 	}
@@ -33,27 +35,18 @@ int conectar(char *ip, int port, t_log * log) {
 	sockaddress.sin_addr.s_addr = inet_addr(ip);
 	sockaddress.sin_port = htons(port);
 
-	if( connect(sock, (struct sockaddr*)&sockaddress, sizeof(struct sockaddr))){
+	if (connect(sock, (struct sockaddr*) &sockaddress,
+			sizeof(struct sockaddr))) {
 		log_error(log, "Error al conectar el socket");
 		return -1;  //No se pudo conectar
 	}
 
-
 	return sock;
 }
 
-
-/*
- *	Funcion que crear un nuevo servidor y queda a la escucha de nuevas conexiones.
- *	Al haber un nuevo cliente, crea un nuevo thread llamando a la fn_nuevo_cliente
- *	con parametro el nuevo socket del nuevo cliente.
- *
- */
-int crearServidor(int puerto, void* (*fn_nuevo_cliente)( void * socket ), t_log * log ){
-
-
-	int socketEscucha, socketNuevaConexion;
-
+int crearYbindearSocket(int puerto, t_log * log)
+{
+	int socketEscucha;
 	struct sockaddr_in socketInfo;
 	int optval = 1;
 
@@ -61,39 +54,58 @@ int crearServidor(int puerto, void* (*fn_nuevo_cliente)( void * socket ), t_log 
 	// AF_INET: Socket de internet IPv4
 	// SOCK_STREAM: Orientado a la conexion, TCP
 	// 0: Usar protocolo por defecto para AF_INET-SOCK_STREAM: Protocolo TCP/IPv4
-	if ((socketEscucha = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-		log_error( log, ("Error al crear el socket"));
+	if ((socketEscucha = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+		log_error(log, ("Error al crear el socket"));
 		return -1;
 	}
 
 	// Hacer que el SO libere el puerto inmediatamente luego de cerrar el socket.
 	setsockopt(socketEscucha, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
 
+	memset(&socketInfo, 0, sizeof(socketInfo));
 	socketInfo.sin_family = AF_INET;
 	socketInfo.sin_addr.s_addr = INADDR_ANY; //Notar que aca no se usa inet_addr()
 	socketInfo.sin_port = htons(puerto);
 
 	// Vincular el socket con una direccion de red almacenada en 'socketInfo'.
-	if (bind(socketEscucha, (struct sockaddr*) &socketInfo, sizeof(socketInfo)) != 0) {
-		log_error( log, ("Error al bindear socket escucha"));
+	if (bind(socketEscucha, (struct sockaddr*) &socketInfo, sizeof(socketInfo))
+			!= 0) {
+		log_error(log, "Error al bindear socket escucha");
 		return -1;
 	}
 
+	return socketEscucha;
+}
+
+/*
+ *	Funcion que crear un nuevo servidor y queda a la escucha de nuevas conexiones.
+ *	Al haber un nuevo cliente, crea un nuevo thread llamando a la fn_nuevo_cliente
+ *	con parametro el nuevo socket del nuevo cliente.
+ *
+ */
+int crearServidor(int puerto, void* (*fn_nuevo_cliente)(void * socket), t_log * log) {
+
+	int socketEscucha, socketNuevaConexion;
+
+	socketEscucha = crearYbindearSocket(puerto, log);
+
+	if(socketEscucha == -1)
+		return -1;
+
 	// Escuchar nuevas conexiones entrantes.
-	if (listen(socketEscucha, 10) != 0) {
-		log_error( log, ("Error al poner a escuchar socket"));
+	if (listen(socketEscucha, SOMAXCONN) != 0) {
+		log_error(log, "Error al poner a escuchar socket");
 		return -1;
 
 	}
 
 	log_info(log, "Escuchando conexiones entrantes...");
 
-
-	while(1){
+	while (1) {
 
 		// Aceptar una nueva conexion entrante. Se genera un nuevo socket con la nueva conexion.
 		if ((socketNuevaConexion = accept(socketEscucha, NULL, 0)) < 0) {
-			log_error( log, ("Error al aceptar conexion entrante"));
+			log_error(log, "Error al aceptar conexion entrante");
 			return -1;
 		}
 
@@ -106,4 +118,99 @@ int crearServidor(int puerto, void* (*fn_nuevo_cliente)( void * socket ), t_log 
 
 	return 0;
 
+}
+
+int crearServidorNoBloqueante(int puerto, bool (*fn_nuevo_mensaje)(void *socket), t_log * log) {
+	int i, rc, optval = 1;
+	int socketEscucha, max_sd, new_sd;
+	bool desc_ready, end_server = false;
+	int close_conn;
+	char buffer[80];
+	fd_set master_set, working_set;
+
+	socketEscucha = crearYbindearSocket(puerto, log);
+
+	if(socketEscucha == -1)
+		return -1;
+
+	// Hacer que el socket se comporte como no bloqueante
+	ioctl(socketEscucha, FIONBIO, &optval);
+
+	// Escuchar nuevas conexiones entrantes.
+	if (listen(socketEscucha, SOMAXCONN) != 0) {
+		log_error(log, "Error al poner a escuchar socket");
+		return -1;
+	}
+
+	///////////////////////////////////
+	FD_ZERO(&master_set);
+	max_sd = socketEscucha;
+	FD_SET(socketEscucha, &master_set);
+
+	do {
+		memcpy(&working_set, &master_set, sizeof(master_set));
+
+		log_info(log, "Esperando en el select algun evento...");
+		rc = select(max_sd + 1, &working_set, NULL, NULL, NULL );
+
+		if (rc <= 0) {
+			log_error(log, "Select fallo o termino su tiempo de espera");
+			break;
+		}
+
+		desc_ready = rc;
+		for (i = 0; i <= max_sd && desc_ready > 0; ++i) {
+			//Viendo si el descriptor se encuentra listo
+			if (FD_ISSET(i, &working_set)) {
+				//Se encontraron descriptores listos para leer
+				desc_ready -= 1;
+
+				//Ver si se trata de una conexion pendiente
+				if (i == socketEscucha) {
+					//Aceptando conexiones pendientes
+					do {
+						new_sd = accept(socketEscucha, NULL, NULL );
+						if (new_sd < 0) {
+							if (errno != EWOULDBLOCK) {
+								log_error(log, "Error al aceptar conexion entrante");
+								end_server = true;
+							}
+							break;
+						}
+
+						FD_SET(new_sd, &master_set);
+						if (new_sd > max_sd)
+							max_sd = new_sd;
+
+						//Nuevo cliente conectado
+					} while (new_sd != -1);
+				}
+
+				//No se trata de una conexion pendiente, si no de una que ya existia previamente
+				else {
+					//Obtener paquetes recibidos
+					close_conn = !fn_nuevo_mensaje(i);
+
+					//Si es necesario, borrar conexion actual
+					if (close_conn) {
+						close(i);
+						FD_CLR(i, &master_set);
+						if (i == max_sd) {
+							while (FD_ISSET(max_sd, &master_set) == false)
+								max_sd -= 1;
+						}
+					}
+				}
+			}
+		}
+
+	} while (end_server == false);
+
+	//Cerrar todas las conexiones que quedaron abiertas
+	for (i = 0; i <= max_sd; ++i) {
+		if (FD_ISSET(i, &master_set))
+			close(i);
+	}
+
+	return -1;
 }
