03142c44b68ed158c44abd488da7a9328a314a1e
FacuPal <facundo.palladino@gmail.com>
2014-05-02 17:32:29 -0300

    terminado, compila, pero falta testear para su correcto funcionamiento

--- Kernel/src/io.c
+++ Kernel/src/io.c
@@ -1,36 +1,46 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
 
 #include "io.h"
+#include "colas.c"
+#include "commons/pcb.h"
 
 struct data_cola{
-	uint32_t id_pcb;
-	int tiempo;
+	uint32_t pid;
+	uint32_t tiempo;
 };
 typedef struct data_cola data_cola_t;
 
 void *hilo_io(void *ptr){
 	io_t *parametros = (io_t *) ptr;
+	pthread_mutex_lock(&parametros->mutex);
 
-	for(;;){
-		pthread_mutex_lock(&parametros->mutex);
-		if (queue_is_empty(parametros->cola))
-			pthread_cond_wait(&parametros->condition,&parametros->mutex);
+	while(1){
 
-	//saco el primer elemento de la cola para activarlo
-	//data_cola_t *elemento_activo = queue_pop(parametros->cola);
+	pthread_cond_wait(&parametros->condition,&parametros->mutex);
 
-	//hay que ver el tiempo ese, porque es en segundos!!!!
-	//sleep(elemento_activo->tiempo);
+	//saco el primer elemento de la cola del dispositivo para activarlo
+	data_cola_t *orden_activa = queue_pop(parametros->cola);
+
+	//aplico el retardo
+	usleep((orden_activa->tiempo) * (parametros->retardo));
+
+	//funcion usada como condicion para buscar el pcb correspondiente en la blockQueue
+	bool matchearPCB (pcb_t *pcb){
+		return (pcb->id == orden_activa->pid);
+	}
 
 	//Saco el pcb de la cola de bloqueados y la pongo en la cola de ready
-	//pcb_t pcb_blocked_to_ready = list_find(BlockedQueue,condicion);
-	//list_remove(BlockedQueue, indexComoSeBusque);
-	//queue_push(ReadyQueue,pcb_blocked_to_ready);
+	pcb_t *pcb_blocked_to_ready = list_remove_by_condition(blockQueue->elements,matchearPCB);
 
-	pthread_mutex_unlock(&parametros->mutex);
+	queue_push(readyQueue,pcb_blocked_to_ready);
+
+
 
 	}
+	pthread_mutex_unlock(&parametros->mutex);
 
 	return(0);
 }
@@ -47,16 +57,28 @@ t_list *armar_lista_dispositivos (char** hioId, char** hioRetardo){
 	t_list *lista_dispositivos;
 	lista_dispositivos = list_create();
 
-	if(sizeof(hioId)){
-		int i = 0;
+	//if(sizeof(hioId)){
+	//	int i = 0;
 
-		for(;i<sizeof(hioId);i++){
-			io_t *registro_hio = crear_registro(hioId[i], hioRetardo[i]);
-			list_add(lista_dispositivos,registro_hio);
-		}
+	//	for(;i<(sizeof(hioId)-1);i++){
+	//		io_t *registro_hio = crear_registro(hioId[i], hioRetardo[i]);
+	//		list_add(lista_dispositivos,registro_hio);
+	//	}
 
 
+	//}
+
+	int i ;
+
+	for(i=0;hioId[i] != NULL;i++){
+		io_t *registro_hio = crear_registro(hioId[i], hioRetardo[i]);
+		list_add(lista_dispositivos,registro_hio);
 	}
+
+
+
+
+
 	return (lista_dispositivos);
 }
 
@@ -74,5 +96,5 @@ io_t *crear_registro(char* hioId, char* hioRetardo){
 	pthread_cond_init(&nuevo_registro->condition,NULL);
 	pthread_create(&(nuevo_registro->thread), NULL, &hilo_io,(void *)nuevo_registro);
 
-	return (nuevo_registro);
+	return nuevo_registro;
 }
