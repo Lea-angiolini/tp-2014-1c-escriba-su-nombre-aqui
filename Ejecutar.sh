1373fda593abf1ce6e184c237f9975cf9cace5b9
Lea-angiolini <angiolini.leandro@gmail.com>
2014-06-09 05:19:28 -0300

    Arreglado bug bash

--- Ejecutar.sh
+++ Ejecutar.sh
@@ -2,36 +2,34 @@
 
 ubicacion=$(pwd)
 clear
-echo "Ingrese cantidad de CPU (max  8)"
+echo "Ingrese cantidad de CPU "
 read n
+clear
 
 cd $ubicacion
 cd UMV/Debug
-xterm -T UMV -hold -e "./UMV ../resources/config.cfg" &
-sleep 0.5
+xterm -T UMV -hold -geometry "120x25" -e "./UMV ../resources/config.cfg" &
+sleep 0.1
 
 cd $ubicacion
 cd Kernel/Debug
-xterm -T Kernel -hold -e "./Kernel ../resources/config.cfg" &
-sleep 0.5
+xterm -T Kernel -hold -geometry "120x25" -e "./Kernel ../resources/config.cfg" &
+sleep 0.2
 
 cant=1
 
-if [ $n -gt 8 ]; then
-   n=8
-fi
-
 cd $ubicacion
 cd CPU/Debug
+
 while [ $cant -le $n ]; do
-	xterm -T CPU$cant -hold -e "./CPU ../resources/config.cfg" &
-    	let cant=$cant+1
+		xterm -T CPU$cant -hold -geometry "120x30" -e "./CPU ../resources/config.cfg" &
+    		let cant=$cant+1
 done
 
 
 cd $ubicacion
 cd Programa/resources
-echo "Ingrese script a ejecutar (no hace falta .ansisop)"
+echo "Ingrese script a ejecutar (no hace falta .ansisop). Ingrese '0' para salir"
 echo " "
 ls
 read s
@@ -41,18 +39,20 @@ while [ $s != 0 ]; do
 		cd $ubicacion
 		cd Programa/Debug
 		export ANSISOP_CONFIG=../resources/config.cfg
-		xterm -T $s -hold -e "./Programa ../resources/$s.ansisop" &
-			
+		xterm -T $s -hold -geometry "120x25" -e "./Programa ../resources/$s.ansisop" &
+		
 		clear
 		cd $ubicacion
 		cd Programa/resources
-		echo "Ingrese script a ejecutar (no hace falta .ansisop)"
+		echo "Ingrese script a ejecutar (no hace falta .ansisop). Ingrese '0' para salir"
 		echo " "
 		ls
 		read s
 
-	done
+done
 
+ps ax | grep xterm | grep resources/config.cfg | awk '{print $1}' | xargs kill
+ps ax | grep xterm | grep ansisop | awk '{print $1}' | xargs kill
 clear
-exit 0
 
+exit 0
