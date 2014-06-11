59d2a8c1282881ede9257175deb4b568a6e57fb3
Lea-angiolini <angiolini.leandro@gmail.com>
2014-06-11 01:54:41 -0300

    Actualizando archivos mk de UMV

--- UMV/Debug/src/commons/collections/subdir.mk
+++ UMV/Debug/src/commons/collections/subdir.mk
@@ -23,21 +23,21 @@ C_DEPS += \
 src/commons/collections/dictionary.o: /home/utnso/git/tp-2014-1c-escriba-su-nombre-aqui/commons/commons/collections/dictionary.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I"/home/utnso/git/tp-2014-1c-escriba-su-nombre-aqui/commons" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc -I"/home/utnso/git/tp-2014-1c-escriba-su-nombre-aqui/commons" -O0 -g3 -Wall -c -fmessage-length=0 -pthread -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 src/commons/collections/list.o: /home/utnso/git/tp-2014-1c-escriba-su-nombre-aqui/commons/commons/collections/list.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I"/home/utnso/git/tp-2014-1c-escriba-su-nombre-aqui/commons" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc -I"/home/utnso/git/tp-2014-1c-escriba-su-nombre-aqui/commons" -O0 -g3 -Wall -c -fmessage-length=0 -pthread -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 src/commons/collections/queue.o: /home/utnso/git/tp-2014-1c-escriba-su-nombre-aqui/commons/commons/collections/queue.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I"/home/utnso/git/tp-2014-1c-escriba-su-nombre-aqui/commons" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc -I"/home/utnso/git/tp-2014-1c-escriba-su-nombre-aqui/commons" -O0 -g3 -Wall -c -fmessage-length=0 -pthread -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- UMV/Debug/src/subdir.mk
+++ UMV/Debug/src/subdir.mk
@@ -4,20 +4,41 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../src/UMV.c 
+../src/CPU.c \
+../src/Consola.c \
+../src/Kernel.c \
+../src/Programa.c \
+../src/Segmento.c \
+../src/UMV.c \
+../src/memoria.c \
+../src/mocks.c 
 
 OBJS += \
-./src/UMV.o 
+./src/CPU.o \
+./src/Consola.o \
+./src/Kernel.o \
+./src/Programa.o \
+./src/Segmento.o \
+./src/UMV.o \
+./src/memoria.o \
+./src/mocks.o 
 
 C_DEPS += \
-./src/UMV.d 
+./src/CPU.d \
+./src/Consola.d \
+./src/Kernel.d \
+./src/Programa.d \
+./src/Segmento.d \
+./src/UMV.d \
+./src/memoria.d \
+./src/mocks.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I"/home/utnso/git/tp-2014-1c-escriba-su-nombre-aqui/commons" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc -I"/home/utnso/git/tp-2014-1c-escriba-su-nombre-aqui/commons" -O0 -g3 -Wall -c -fmessage-length=0 -pthread -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
